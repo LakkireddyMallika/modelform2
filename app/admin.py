04d456a0bbdd3aab95d0bea32e8864321ab58732
LakkireddyMallika <lmallikacareer@gmail.com>
2022-12-27 14:00:45 +0530

    first commit

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 
 # Register your models here.
 from app.models import *
+class WebpageAdminView(admin.ModelAdmin):
+    list_display=['topic_name','name','url']
+    #list_editable=['name']
+    list_display_links=['topic_name','name']
+    search_fields=['name']
+    list_per_page=2
+    list_filter=['name','topic_name']
+
+
+
+
+
+
+
 
 admin.site.register(Topic)
-admin.site.register(Webpage)
+admin.site.register(Webpage,WebpageAdminView)
 admin.site.register(AccessRecords)
--- app/forms.py
+++ app/forms.py
@@ -4,11 +4,18 @@ class TopicForm(forms.ModelForm):
     class Meta:
         model=Topic
         fields='__all__'
+
     
 class WebpageForm(forms.ModelForm):
     class Meta:
         model=Webpage
         fields='__all__'
+        fields=['topic_name','name','url']
+        #exclude=['name']
+        help_texts={'topic_name':'should not be integers','name':'only Alphabets'}
+        labels={'topic_name':'TN','name':'N'}
+        #widgets={'url':forms.PasswordInput,'name':forms.Textarea}
+    
 
 
 class AccessRecordsForm(forms.ModelForm):
